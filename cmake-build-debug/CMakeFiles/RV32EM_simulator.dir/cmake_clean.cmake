4af872cc120bb3206dd090105e9d047fd137ec56
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-06-01 18:40:12 -0400

    first commit of a empty project

---
+++ cmake-build-debug/CMakeFiles/RV32EM_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RV32EM_simulator.dir/main.c.obj"
+  "RV32EM_simulator.exe"
+  "RV32EM_simulator.exe.manifest"
+  "RV32EM_simulator.pdb"
+  "libRV32EM_simulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RV32EM_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
