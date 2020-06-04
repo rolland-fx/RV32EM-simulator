39ade3e399f8ef2019a282275161525f2607b95d
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-06-04 13:51:25 -0400

    - Adding code for AfficheInstruction and AfficheEtatCourant - AfficheInstruction will need information from decoder to show correct information - AfficheEtatCourant should be fully working

--- src/main.c
+++ src/main.c
@@ -10,9 +10,18 @@ void AffichageInitial()
     printf("- Jean-Pascal Choiniere\n");
     printf("2020\n");
 }
+void AfficheInstruction()
+{
+    printf("Instruction : 0x%08x [xxxxxxxxxxxxxxx]",15);
+
+}
 
 void AfficheEtatCourant()
 {
+    printf("PC : 0x%08x x1 : 0x%08x x2 : 0x%08x x3 : 0x%08x\n", Register[16],Register[1],Register[2],Register[3]);
+    printf("x4 : 0x%08x x5 : 0x%08x x6 : 0x%08x x7 : 0x%08x\n", Register[4],Register[5],Register[6],Register[7]);
+    printf("x8 : 0x%08x x9 : 0x%08x x10 : 0x%08x x11 : 0x%08x\n", Register[8],Register[9],Register[10],Register[11]);
+    printf("x12 : 0x%08x x13 : 0x%08x x14 : 0x%08x x15 : 0x%08x\n", Register[12],Register[13],Register[14],Register[15]);
 }
 
 
@@ -28,8 +37,12 @@ char CodeRun() {
         }
         ManageRegisterPC();
         NextInstructionExist = RunNextInstruction();
-        if (choix == 's') AfficheEtatCourant();
+        if (choix == 's') {
+            AfficheInstruction();
+            AfficheEtatCourant();
+        }
     }
+    AfficheInstruction();
     AfficheEtatCourant();
     return 0;
 }
