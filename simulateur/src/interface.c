2d503122608b6f351d199cc0bc08a9829fa3c1ce
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-06-16 14:19:40 -0400

    The instructions (in hexa) shown in the interface is now the current one, and not a place holder

--- simulateur/src/interface.c
+++ simulateur/src/interface.c
@@ -13,17 +13,17 @@ void InitialPrint()
     printf("- F-X Rolland\n");
     printf("2020\n");
 }
-void PrintInstruction()
+void PrintInstruction(uint32_t CurrentCodedInstruction)
 {
-    printf("Instruction : 0x%08x [xxxxxxxxxxxxxxx]\n",0x00a12023);
+    printf("Instruction : 0x%08X [xxxxxxxxxxxxxxx]\n",CurrentCodedInstruction);
 }
 
 void PrintCurrentState()
 {
-    printf("PC : 0x%08x x1 : 0x%08x x2 : 0x%08x x3 : 0x%08x\n", PC,Register[1],Register[2],Register[3]);
-    printf("x4 : 0x%08x x5 : 0x%08x x6 : 0x%08x x7 : 0x%08x\n", Register[4],Register[5],Register[6],Register[7]);
-    printf("x8 : 0x%08x x9 : 0x%08x x10: 0x%08x x11: 0x%08x\n", Register[8],Register[9],Register[10],Register[11]);
-    printf("x12: 0x%08x x13: 0x%08x x14: 0x%08x x15: 0x%08x\n", Register[12],Register[13],Register[14],Register[15]);
+    printf("PC : 0x%08X x1 : 0x%08X x2 : 0x%08X x3 : 0x%08X\n", PC,Register[1],Register[2],Register[3]);
+    printf("x4 : 0x%08X x5 : 0x%08X x6 : 0x%08X x7 : 0x%08X\n", Register[4],Register[5],Register[6],Register[7]);
+    printf("x8 : 0x%08X x9 : 0x%08X x10: 0x%08X x11: 0x%08X\n", Register[8],Register[9],Register[10],Register[11]);
+    printf("x12: 0x%08X x13: 0x%08X x14: 0x%08X x15: 0x%08X\n", Register[12],Register[13],Register[14],Register[15]);
 }
 
 void PrintSliceofMemory(uint32_t address, uint8_t MemoryCaseToShow)
@@ -33,8 +33,8 @@ void PrintSliceofMemory(uint32_t address, uint8_t MemoryCaseToShow)
     {
         if(i < MEMORY_SIZE)
         {
-            //printf("0x%08x : 0x%08x\n",i,Memory[i]);
-            printf("0x%08x : 0x%08x\n",i,user_memory_get_word(i));
+            //printf("0x%08X : 0x%08X\n",i,Memory[i]);
+            printf("0x%08X : 0x%08X\n",i,user_memory_get_word(i));
         }
     }
     printf("\n\n");
