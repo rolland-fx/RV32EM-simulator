0cff1c7c04c19046ea0d5c60bf43b6b2247a340e
Francois-Xavier Rolland <rolland.fx@gmail.com>
2020-06-22 15:12:24 -0400

    BGEU and BGLU added

--- simulateur/src/execute/execute_B.c
+++ simulateur/src/execute/execute_B.c
@@ -120,6 +120,65 @@ uint8_t execute_type_B_BGE(struct_B* ptr_struct){
     return retVal;
 }
 
+uint8_t execute_type_B_BLTU(struct_B* ptr_struct){
+    uint8_t retVal = 0;
+    uint32_t imm = (ptr_struct->imm_4_1 << 1u) | (ptr_struct->imm_10_5 << 5u)
+                   | (ptr_struct->imm_11 << 11u) | (ptr_struct->imm_12 << 12u);
+
+    if (imm & 0x00001000u)
+    {
+        imm = 0xFFFFE000u | imm;
+    }
+    else
+    {
+        imm = 0x00000000u | imm;
+    }
+
+    if(ptr_struct->rs1 < 16 && ptr_struct->rs2 < 16){
+        if((uint32_t)abs(Register[ptr_struct->rs1]) < (uint32_t)abs(Register[ptr_struct->rs2]))
+        {
+            PC += imm;
+        } else{
+            PC += 4;
+        }
+    }
+    else{
+        retVal = 1;
+    }
+
+    return retVal;
+}
+
+uint8_t execute_type_B_BGEU(struct_B* ptr_struct){
+    uint8_t retVal = 0;
+    uint32_t imm = (ptr_struct->imm_4_1 << 1u) | (ptr_struct->imm_10_5 << 5u)
+                   | (ptr_struct->imm_11 << 11u) | (ptr_struct->imm_12 << 12u);
+
+    if (imm & 0x00001000u)
+    {
+        imm = 0xFFFFE000u | imm;
+    }
+    else
+    {
+        imm = 0x00000000u | imm;
+    }
+
+    if(ptr_struct->rs1 < 16 && ptr_struct->rs2 < 16){
+        if((uint32_t)abs(Register[ptr_struct->rs1]) >= (uint32_t)abs(Register[ptr_struct->rs2]))
+        {
+            PC += imm;
+        } else{
+            PC += 4;
+        }
+    }
+    else{
+        retVal = 1;
+    }
+
+    return retVal;
+}
+
+
 uint8_t execute_type_B(struct_B* ptr_struct) {
     uint8_t return_val = 0;
 
@@ -137,8 +196,10 @@ uint8_t execute_type_B(struct_B* ptr_struct) {
             return_val = execute_type_B_BGE(ptr_struct);
             break;
         case BLTU_FUNCT3: //BLTU
+            return_val = execute_type_B_BLTU(ptr_struct);
             break;
         case BGEU_FUNCT3: //BGEU
+            return_val = execute_type_B_BGEU(ptr_struct);
             break;
         default:
             return_val = 1;
